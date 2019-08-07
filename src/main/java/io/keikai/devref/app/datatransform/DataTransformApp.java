c6e3d3204fea31718297512ef30e6a4271bf557c
MDuchemin <matthieu@potix.com>
2019-08-07 16:06:12 +0800

    workflow first commit

--- src/main/java/io/keikai/devref/app/datatransform/DataTransformApp.java
+++ src/main/java/io/keikai/devref/app/datatransform/DataTransformApp.java
@@ -12,16 +12,38 @@ import java.util.*;
 public class DataTransformApp implements UseCase {
 	
 	private Spreadsheet mainSpreadsheet;
+<<<<<<< HEAD
 	
+=======
+	private Spreadsheet hiddenspreadsheet;
+	
+	private static final String SOURCE_BOOK_NAME = "source.xlsx";
+	private static final String DISPLAY_BOOK_NAME = "display.xlsx";
+>>>>>>> workflow first commit
 	private static final String TRANSFORM_BOOK_NAME = "transform.xlsx";
 	
 	private static final String BOOK_FOLDER = Configuration.DEFAULT_BOOK_FOLDER + "/data-transform";
 	
+<<<<<<< HEAD
+=======
+	private static final String[] INPUT_FIELD_NAMES;
+	private static final String[] OUTPUT_FIELD_NAMES;
+	
+	static {
+		INPUT_FIELD_NAMES = new String[] {"data1","data2","data3","data4","data5"};
+		OUTPUT_FIELD_NAMES = new String[] {"output1","output2","output3","output4","output5","output6"};
+	}
+
+>>>>>>> workflow first commit
 	/* Initialization code for the client engine */
 	@Override
 	public void init(String keikaiEngineAddress) {
 		/* Open a client connection to the server located at keikaiEngineAddress */
 		mainSpreadsheet = Keikai.newClient(keikaiEngineAddress);
+<<<<<<< HEAD
+=======
+		hiddenspreadsheet = Keikai.newClient(keikaiEngineAddress);
+>>>>>>> workflow first commit
 		/* Sets up callbacks for the browser. */
 		mainSpreadsheet.setUIActivityCallback(new UIActivity() {
 
@@ -40,12 +62,21 @@ public class DataTransformApp implements UseCase {
 	}
 
 	private void setupUi() {
+<<<<<<< HEAD
 		mainSpreadsheet.getUIService().showToolbar(false);
 		mainSpreadsheet.getUIService().showSheetTabs(false);
 		mainSpreadsheet.getUIService().showContextMenu(true);
 		mainSpreadsheet.getUIService().showFormulaBar(false);
 		mainSpreadsheet.getUIService().showSheetControls(false);
 		mainSpreadsheet.getUIService().setProtectedSheetWarningEnabled(false);
+=======
+		mainSpreadsheet.getUIService().showToolbar(true);
+		mainSpreadsheet.getUIService().showSheetTabs(true);
+		mainSpreadsheet.getUIService().showContextMenu(true);
+		mainSpreadsheet.getUIService().showFormulaBar(true);
+		mainSpreadsheet.getUIService().showSheetControls(true);
+		mainSpreadsheet.getUIService().setProtectedSheetWarningEnabled(true);
+>>>>>>> workflow first commit
 	}
 
 	@Override
@@ -57,6 +88,7 @@ public class DataTransformApp implements UseCase {
 	public void run() {
 		try {
 			/* Import the spreadsheet from file for each new client */
+<<<<<<< HEAD
 			mainSpreadsheet.importAndReplace(TRANSFORM_BOOK_NAME, new File(BOOK_FOLDER, TRANSFORM_BOOK_NAME));
 			/* set payrollSheet as the active (visible) sheet in client */
 			mainSpreadsheet.setActiveWorkbook(TRANSFORM_BOOK_NAME);
@@ -68,14 +100,75 @@ public class DataTransformApp implements UseCase {
 			});
 			sourceSheet.getButton("toDisplay2").addAction(buttonShapeMouseEvent -> {
 				goToSheet("Display2");
+=======
+			mainSpreadsheet.importAndReplace(SOURCE_BOOK_NAME, new File(BOOK_FOLDER, SOURCE_BOOK_NAME));
+			/* set payrollSheet as the active (visible) sheet in client */
+			mainSpreadsheet.setActiveWorkbook(SOURCE_BOOK_NAME);
+			mainSpreadsheet.getWorkbook(SOURCE_BOOK_NAME).setActiveWorksheet("Sheet1");
+			Worksheet sourceSheet = mainSpreadsheet.getWorkbook(SOURCE_BOOK_NAME).getWorksheet();
+			/* register event listener on the button named "confirmButton" */
+			sourceSheet.getButton("commitButton").addAction(buttonShapeMouseEvent -> {
+				doDataProcess();
+>>>>>>> workflow first commit
 			});
 		} catch (FileNotFoundException | AbortedException e) {
 			e.printStackTrace();
 		}
 	}
 
+<<<<<<< HEAD
 	private void goToSheet(String target) {
 		mainSpreadsheet.getWorkbook().getWorksheet(target).activate();
 	}
 
+=======
+	private void doDataProcess() throws FileNotFoundException, AbortedException {
+		Map<String,Object> inputData = retrieveDataFromSourceSheet();
+		pushDataToTransformSheet(inputData);
+		Map<String,Object> outputData = retrieveDataFromTransformSheet();
+		pushDataToDisplaySheet(outputData);
+		mainSpreadsheet.setActiveWorkbook(DISPLAY_BOOK_NAME);
+		mainSpreadsheet.getWorkbook(DISPLAY_BOOK_NAME).setActiveWorksheet(0);
+	}
+	private Map<String, Object> retrieveDataFromSourceSheet() {
+		return retrieveData(mainSpreadsheet, INPUT_FIELD_NAMES);
+	}
+		
+	private Map<String, Object> retrieveDataFromTransformSheet() {
+		Map<String, Object> resultData = retrieveData(hiddenspreadsheet, OUTPUT_FIELD_NAMES);
+		hiddenspreadsheet.close();
+		return resultData;
+	}
+
+	private Map<String, Object> retrieveData(Spreadsheet targetSpreadSheet, String[] fieldList) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		for (String field : fieldList) {
+			result.put(field, targetSpreadSheet.getRangeByName(field).getValue());
+		}
+		return result;
+	}
+	
+	private void pushDataToTransformSheet(Map<String, Object> inputData) throws FileNotFoundException, AbortedException {
+		hiddenspreadsheet.importAndReplace(TRANSFORM_BOOK_NAME, new File(BOOK_FOLDER, TRANSFORM_BOOK_NAME));
+		hiddenspreadsheet.setActiveWorkbook(TRANSFORM_BOOK_NAME);
+		hiddenspreadsheet.getWorkbook(TRANSFORM_BOOK_NAME).setActiveWorksheet("Sheet1");
+		pushData(hiddenspreadsheet, inputData, INPUT_FIELD_NAMES);
+	}
+
+	private void pushDataToDisplaySheet(Map<String, Object> outputData) throws FileNotFoundException, AbortedException {
+		mainSpreadsheet.importAndReplace(DISPLAY_BOOK_NAME, new File(BOOK_FOLDER, DISPLAY_BOOK_NAME));
+		mainSpreadsheet.setActiveWorkbook(DISPLAY_BOOK_NAME);
+		mainSpreadsheet.getWorkbook(DISPLAY_BOOK_NAME).setActiveWorksheet("Sheet1");
+		pushData(mainSpreadsheet, outputData, OUTPUT_FIELD_NAMES);
+	}
+
+	private void pushData(Spreadsheet targetSpreadSheet, Map<String, Object> data, String[] fieldList) {
+		for (String field : fieldList) {
+			targetSpreadSheet.getRangeByName(field).setValue(data.get(field));
+		}
+	}
+
+
+
+>>>>>>> workflow first commit
 }
