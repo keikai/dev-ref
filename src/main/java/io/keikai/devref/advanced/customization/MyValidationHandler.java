f6a36c4c16a704af0e0a16859af21dd278704d54
hawkchen <hawkchen@potix.com>
2020-03-10 15:00:14 +0800

    minor

--- src/main/java/io/keikai/devref/advanced/customization/MyValidationHandler.java
+++ src/main/java/io/keikai/devref/advanced/customization/MyValidationHandler.java
@@ -1,17 +1,19 @@
 package io.keikai.devref.advanced.customization;
 
+import io.keikai.api.AreaRef;
+import io.keikai.api.model.*;
 import io.keikai.ui.UserActionContext;
 import io.keikaiex.ui.impl.ua.DataValidationHandler;
+
+import java.util.List;
 
 /**
  * A dummy example that shows you can extend an existing {@link io.keikai.ui.UserActionHandler}
  */
 public class MyValidationHandler extends DataValidationHandler {
 
-    //override a method
     @Override
-    protected boolean processAction(final UserActionContext ctx) {
-        return super.processAction(ctx);
+    protected void showValidationDialog(UserActionContext ctx, Sheet sheet, AreaRef selection, List<Validation> validations) {
+        super.showValidationDialog(ctx, sheet, selection, validations);
     }
-
 }
