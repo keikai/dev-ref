150149a0cb63a408e353479a3e5c623da0cd1b74
hawkchen <hawkchen@potix.com>
2020-04-09 10:46:03 +0800

    add an example to wire an component

--- src/main/java/io/keikai/devref/jsf/MyController.java
+++ src/main/java/io/keikai/devref/jsf/MyController.java
@@ -1,25 +1,35 @@
 package io.keikai.devref.jsf;
 
-import javax.faces.bean.*;
-import javax.faces.context.FacesContext;
-
+import io.keikai.ui.Spreadsheet;
 import io.keikai.ui.event.*;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
-import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.*;
 import org.zkoss.zk.ui.util.Clients;
+
+import javax.faces.bean.*;
+import javax.faces.context.FacesContext;
 
 @ManagedBean(name= "myController")
 @RequestScoped
 public class MyController extends SelectorComposer<Component>{
 	private static final long serialVersionUID = 1L;
 
+	/** refer to {@link SelectorComposer} for more syntax of component selector **/
+	@Wire("spreadsheet")
+	private Spreadsheet spreadsheet;
+
+	/**
+	 * Initialize keikai in this method, e.g. import an xlsx file and manipulate Book object for initialization, set spreadsheet's properties,
+	 */
 	@Override
 	public void doAfterCompose(Component comp) throws Exception {
 		super.doAfterCompose(comp);
-		// import an xlsx file
-		// manipulate Book object for initialization
-		System.out.println("initialize cell data");
+		spreadsheet.setShowFormulabar(false);
+		spreadsheet.setShowToolbar(false);
+		spreadsheet.setShowSheetbar(false);
+		spreadsheet.setMaxVisibleRows(15);
+		spreadsheet.setMaxVisibleColumns(9);
 	}
 
 	@Listen(Events.ON_STOP_EDITING + " = spreadsheet")
