7b0b9b5c0dbb2032d0aff4e8dfa84144717617a7
hawkchen <hawkchen@potix.com>
2020-04-09 11:27:17 +0800

    formatting code

--- src/main/java/io/keikai/devref/jsf/MyController.java
+++ src/main/java/io/keikai/devref/jsf/MyController.java
@@ -10,31 +10,34 @@ import org.zkoss.zk.ui.util.Clients;
 import javax.faces.bean.*;
 import javax.faces.context.FacesContext;
 
-@ManagedBean(name= "myController")
+@ManagedBean(name = "myController")
 @RequestScoped
-public class MyController extends SelectorComposer<Component>{
-	private static final long serialVersionUID = 1L;
+public class MyController extends SelectorComposer<Component> {
+    private static final long serialVersionUID = 1L;
 
-	/** refer to {@link SelectorComposer} for more syntax of component selector **/
-	@Wire("spreadsheet")
-	private Spreadsheet spreadsheet;
+    /**
+     * refer to {@link SelectorComposer} for more syntax of component selector
+     **/
+    @Wire("spreadsheet")
+    private Spreadsheet spreadsheet;
 
-	/**
-	 * Initialize keikai in this method, e.g. import an xlsx file and manipulate Book object for initialization, set spreadsheet's properties,
-	 */
-	@Override
-	public void doAfterCompose(Component comp) throws Exception {
-		super.doAfterCompose(comp);
-		spreadsheet.setShowFormulabar(false);
-		spreadsheet.setShowToolbar(false);
-		spreadsheet.setShowSheetbar(false);
-		spreadsheet.setMaxVisibleRows(15);
-		spreadsheet.setMaxVisibleColumns(9);
-	}
+    /**
+     * Initialize keikai in this method, e.g. import an xlsx file and manipulate Book object for initialization,
+	 * set spreadsheet's properties.
+     */
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+        spreadsheet.setShowFormulabar(false);
+        spreadsheet.setShowToolbar(false);
+        spreadsheet.setShowSheetbar(false);
+        spreadsheet.setMaxVisibleRows(15);
+        spreadsheet.setMaxVisibleColumns(9);
+    }
 
-	@Listen(Events.ON_STOP_EDITING + " = spreadsheet")
-    public void onStopEditing(StopEditingEvent event){
-		FacesContext context = FacesContext.getCurrentInstance();
-		Clients.showNotification("edited");
-	}
+    @Listen(Events.ON_STOP_EDITING + " = spreadsheet")
+    public void onStopEditing(StopEditingEvent event) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        Clients.showNotification("edited");
+    }
 }
