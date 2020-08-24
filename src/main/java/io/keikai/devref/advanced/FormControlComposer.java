14745ecedd915a6cc5c481905d3e87cb05e21c7f
hawkchen <hawkchen@potix.com>
2020-08-24 15:06:37 +0800

    improve form control alternatives example

--- src/main/java/io/keikai/devref/advanced/FormControlComposer.java
+++ src/main/java/io/keikai/devref/advanced/FormControlComposer.java
@@ -1,9 +1,7 @@
 package io.keikai.devref.advanced;
 
-import java.util.*;
-
-import io.keikai.api.Ranges;
-import io.keikai.ui.Spreadsheet;
+import io.keikai.api.*;
+import io.keikai.devref.util.RangeHelper;
 import io.keikai.ui.event.*;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
@@ -16,33 +14,28 @@ import org.zkoss.zul.*;
 public class FormControlComposer extends SelectorComposer<Component> {
 
 	private static final long serialVersionUID = 1L;
-	private Map<String, String> controlsName = new HashMap<String, String>();
-	@Wire
-	private Spreadsheet ss;
+	private static final String CELL = "CELL";
 	@Wire
 	private Popup popup;
+	@Wire
+	private Listbox optionBox;
 	
-	@Override
-	public void doAfterCompose(Component comp) throws Exception {
-		super.doAfterCompose(comp);
-		controlsName.put("✔", "checked");
-		controlsName.put("◻", "box");
-		controlsName.put("✈", "airplane");
-		controlsName.put("✕", "cross");
-		controlsName.put("★", "star");
-		controlsName.put("✜", "plus");
-		controlsName.put("✪", "star shield");
-	}
 
 	@Listen(Events.ON_CELL_CLICK + "= #ss")
-	public void print(CellMouseEvent e){
-		String controlSymbol = Ranges.range(e.getSheet(),e.getRow(), e.getColumn()).getCellEditText();
-		String name = controlsName.get(controlSymbol);
-		if (name != null){
-			Ranges.range(e.getSheet(), e.getRow(), e.getColumn()+1).setCellEditText(name);
-		}else{
+	public void showOptions(CellMouseEvent e){
+		Range clickedCell = RangeHelper.getTargetRange(e);
+		if (e.getColumn() == 2
+			&& clickedCell.getCellData().isBlank()){
 			popup.open(e.getClientx(),e.getClienty());
+			optionBox.setAttribute(CELL, clickedCell);
 		}
+	}
+
+	@Listen(org.zkoss.zk.ui.event.Events.ON_SELECT  + "=#optionBox")
+	public void fillOption(){
+		Range clickedCEll = (Range)optionBox.getAttribute(CELL);
+		clickedCEll.setCellValue(optionBox.getSelectedItem().getValue());
+		optionBox.clearSelection();
 	}
 }
 
