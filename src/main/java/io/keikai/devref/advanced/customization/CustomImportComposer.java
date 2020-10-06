4cdd7ebdc830845df6ef2f57b3b7ae39026ea47c
hawkchen <hawkchen@potix.com>
2020-10-06 16:13:17 +0800

    make custom importer example not to override the default importer

--- src/main/java/io/keikai/devref/advanced/customization/CustomImportComposer.java
+++ src/main/java/io/keikai/devref/advanced/customization/CustomImportComposer.java
@@ -1,20 +1,28 @@
 package io.keikai.devref.advanced.customization;
 
+import io.keikai.api.Importers;
 import io.keikai.devref.util.BookUtil;
+import io.keikai.ui.Spreadsheet;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Wire;
 
 public class CustomImportComposer extends SelectorComposer {
 
+    @Wire
+    private Spreadsheet spreadsheet;
+
     public CustomImportComposer() {
         super();
-        BookUtil.overrideDefaultImporter(FirstSheetImporter::new);
+//        BookUtil.overrideDefaultImporter(FirstSheetImporter::new);
+        BookUtil.registerCustomImporter("first", FirstSheetImporter::new);
     }
 
     @Override
     public void doAfterCompose(Component comp) throws Exception {
         super.doAfterCompose(comp);
-        BookUtil.registerCustomImporter("first", FirstSheetImporter::new);
+        String fileName = "Payroll.xlsx";
+        spreadsheet.setBook(Importers.getImporter("first").imports(BookUtil.getFile(fileName), fileName));
     }
 
 }
