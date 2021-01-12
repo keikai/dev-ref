be325d40e2abe3e040bbe1af50a0c69526bcbc7d
hawkchen <hawkchen@potix.com>
2021-01-12 15:46:54 +0800

    add an example to hide a sheet

--- src/main/java/io/keikai/devref/CopySheetComposer.java
+++ src/main/java/io/keikai/devref/CopySheetComposer.java
@@ -1,9 +1,11 @@
 package io.keikai.devref;
 
+import io.keikai.api.*;
 import io.keikai.api.model.Book;
 import io.keikai.devref.util.BookUtil;
 import io.keikai.ui.Spreadsheet;
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.*;
 
@@ -23,5 +25,9 @@ public class CopySheetComposer extends SelectorComposer<Component> {
 		Book newBook = BookUtil.copySheetToNewBook("newOne", srcSpreadsheet.getSelectedSheet());
 		targetSpreadsheet.setBook(newBook);
 	}
-	
+
+	@Listen(Events.ON_CLICK + "= #hide")
+	public void hideCurrentSheet(){
+		Ranges.range(srcSpreadsheet.getSelectedSheet()).setSheetVisible(Range.SheetVisible.HIDDEN);
+	}
 }
