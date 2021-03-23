76436a53da5e27a2c1390ec10d4d29f61c9086d6
hawkchen <hawkchen@potix.com>
2021-03-23 15:51:57 +0800

    export to pdf in different scopes

--- src/main/java/io/keikai/devref/ExportPdfComposer.java
+++ src/main/java/io/keikai/devref/ExportPdfComposer.java
@@ -2,8 +2,7 @@ package io.keikai.devref;
 
 import java.io.*;
 
-import io.keikai.api.model.Book;
-import io.keikai.model.SPrintSetup;
+import io.keikai.model.*;
 import io.keikai.model.impl.pdf.*;
 import io.keikai.ui.Spreadsheet;
 import org.zkoss.util.media.AMedia;
@@ -16,59 +15,82 @@ import org.zkoss.zul.*;
 
 /**
  * This class demonstrate pdf exporter API
- * @author Hawk
  *
+ * @author Hawk
  */
 public class ExportPdfComposer extends SelectorComposer<Component> {
 
-	@Wire
-	private Spreadsheet ss;
-	@Wire
-	private Listbox fileBox;
+    @Wire
+    private Spreadsheet ss;
+    @Wire
+    private Listbox fileBox;
 
-	@Wire
-	private Window printDialog;
+    @Wire
+    private Window printDialog;
 
-	@Wire("#printDialog #centerH")
-	private Checkbox centerH;
-	@Wire("#printDialog #centerV")
-	private Checkbox centerV;
-	@Wire("#printDialog #printGridlines")
-	private Checkbox printGridlines;
+    @Wire("#printDialog #centerH")
+    private Checkbox centerH;
+    @Wire("#printDialog #centerV")
+    private Checkbox centerV;
+    @Wire("#printDialog #printGridlines")
+    private Checkbox printGridlines;
+    @Wire("#printDialog selectbox")
+    private Selectbox scopeBox;
 
-	//the default way, it uses the file's printing setup
+    enum PrintingScope {BOOK, SHEET, SELECTION}
+
+    private ListModelList<PrintingScope> printingScopeModel = new ListModelList(PrintingScope.values());
+
+    //the default way, it uses the file's printing setup
 //	private Exporter exporter = Exporters.getExporter("pdf");
-	// use this if you need to change print setup
-	private PdfExporter pdfExporter = new PdfExporter();
-	
-	@Listen("onClick = #printDialog #exportPdf")
-	public void doExport(Event event) throws IOException{
-		printDialog.setVisible(false);
-		loadSetup();
+    // use this if you need to change print setup
+    private PdfExporter pdfExporter = new PdfExporter();
 
-		File file = File.createTempFile(Long.toString(System.currentTimeMillis()),"temp");
-		FileOutputStream fos = null;
-		try {
-			fos = new FileOutputStream(file);
-			pdfExporter.export(ss.getBook().getInternalBook(), file);
-		}finally{
-			if(fos!=null){
-				fos.close();
-			}
-		}
-		Filedownload.save(new AMedia(ss.getBook().getBookName()+".pdf", "pdf", "application/pdf", file, true));
-	}
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+        scopeBox.setModel(printingScopeModel);
+        printingScopeModel.addToSelection(PrintingScope.BOOK);
+    }
 
-	@Listen("onSelect = listbox")
-	public void loadFile(){
-		String fileName = fileBox.getSelectedItem().getLabel();
-		ss.setSrc("/WEB-INF/books/"+fileName);
-	}
+    @Listen("onClick = #printDialog #exportPdf")
+    public void doExport(Event event) throws IOException {
+        printDialog.setVisible(false);
+        loadSetup();
 
-	public void loadSetup(){
-		SPrintSetup setup = pdfExporter.getPrintSetup();
-		setup.setHCenter(centerH.isChecked());
-		setup.setVCenter(centerV.isChecked());
-		setup.setPrintGridlines(printGridlines.isChecked());
-	}
+        File file = File.createTempFile(Long.toString(System.currentTimeMillis()), "temp");
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(file);
+            switch (printingScopeModel.getSelection().iterator().next()) {
+                case BOOK:
+                    pdfExporter.export(ss.getBook().getInternalBook(), file);
+                    break;
+                case SHEET:
+                    pdfExporter.export(ss.getSelectedSSheet(), fos);
+                    break;
+                case SELECTION:
+                    pdfExporter.export(new SheetRegion(ss.getSelectedSSheet(), new CellRegion(ss.getSelection().asString())), fos);
+                    break;
+            }
+        } finally {
+            if (fos != null) {
+                fos.close();
+            }
+        }
+        Filedownload.save(new AMedia(ss.getBook().getBookName() + ".pdf", "pdf", "application/pdf", file, true));
+    }
+
+    @Listen("onSelect = listbox")
+    public void loadFile() {
+        String fileName = fileBox.getSelectedItem().getLabel();
+        ss.setSrc("/WEB-INF/books/" + fileName);
+    }
+
+    public void loadSetup() {
+        SPrintSetup setup = pdfExporter.getPrintSetup();
+        setup.setHCenter(centerH.isChecked());
+        setup.setVCenter(centerV.isChecked());
+        setup.setPrintGridlines(printGridlines.isChecked());
+    }
 }
