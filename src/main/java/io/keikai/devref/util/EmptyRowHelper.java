614c7538c99a74b1ed69b510cbae6038a5ef1818
hawkchen <hawkchen@potix.com>
2021-05-10 23:24:31 +0800

    a helper class to find empty rows

---
+++ src/main/java/io/keikai/devref/util/EmptyRowHelper.java
@@ -0,0 +1,28 @@
+package io.keikai.devref.util;
+
+import io.keikai.api.Ranges;
+import io.keikai.api.model.*;
+import io.keikai.model.SRow;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class EmptyRowHelper {
+
+    static public List<Integer> findEmptyRows(Sheet sheet, int maxRow) {
+        List<Integer> emptyRowIndexes = IntStream.rangeClosed(0, maxRow)
+                .boxed().collect(Collectors.toList());
+
+        Iterator<SRow> rowIterator = sheet.getInternalSheet().getRowIterator(0, maxRow);
+        while (rowIterator.hasNext()) {
+            SRow row = rowIterator.next();
+            emptyRowIndexes.remove(Integer.valueOf(row.getIndex()));
+        }
+        return emptyRowIndexes;
+    }
+
+    static public void hideEmptyRows(List<Integer> emptyRowIndexes, Sheet sheet) {
+        for (Integer rowIndex : emptyRowIndexes)
+            Ranges.range(sheet, rowIndex, 0).toRowRange().setHidden(true);
+    }
+}
