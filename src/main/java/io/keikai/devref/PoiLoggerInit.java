6b9333c09f40f28a5f47ddd362ddcf4db610660e
hawkchen <hawkchen@potix.com>
2021-09-23 18:15:10 +0800

    add POI logger settings

---
+++ src/main/java/io/keikai/devref/PoiLoggerInit.java
@@ -0,0 +1,14 @@
+package io.keikai.devref;
+
+import org.zkoss.zk.ui.WebApp;
+import org.zkoss.zk.ui.util.WebAppInit;
+
+/**
+ * Since {@link org.zkoss.poi.util.POILogFactory} uses {@link org.zkoss.poi.util.NullLogger} by default. We need to specify another logger.
+ */
+public class PoiLoggerInit implements WebAppInit {
+    @Override
+    public void init(WebApp wapp) throws Exception {
+        System.setProperty("org.zkoss.poi.util.POILogger", "org.zkoss.poi.util.CommonsLogger");
+    }
+}
