047125c5d809c9075414ae40614b8b85e27f05ae
hawkchen <hawkchen@potix.com>
2021-11-16 15:50:16 +0800

    use case: populate data from a database dynamically, invoke auto-fit from java, so that end users don't need to manually resize every column.

--- src/main/java/io/keikai/devref/component/AutoFitComposer.java
+++ src/main/java/io/keikai/devref/component/AutoFitComposer.java
@@ -1,5 +1,6 @@
 package io.keikai.devref.component;
 
+import io.keikai.api.Ranges;
 import io.keikai.ui.Spreadsheet;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.*;
@@ -13,7 +14,7 @@ public class AutoFitComposer extends SelectorComposer {
     @Wire
     private Intbox endCol;
 
-    @Listen("onClick = button")
+    @Listen("onClick = #autofit")
     public void autoFit(){
         int start = startCol.getValue();
         int end = endCol.getValue();
@@ -25,5 +26,19 @@ public class AutoFitComposer extends SelectorComposer {
         }else{
             spreadsheet.setAutofitColumnWidth(spreadsheet.getSelectedSheet().getInternalSheet(), start, end);
         }
+    }
+
+    /**
+     * use case: populate data from a database dynamically, invoke auto-fit from java, so that end users don't need to manually resize every column.
+     */
+    @Listen("onClick = #populate")
+    public void populateAutoFit(){
+        int maxColumn = 9;
+        for (int row = 0; row <100 ; row ++){
+            for (int col = 0 ; col <= maxColumn ; col++){
+                Ranges.range(spreadsheet.getSelectedSheet(), row, col).setCellValue(String.format("data loaded dynamically %s-%s", row, col));
+            }
+        }
+        spreadsheet.setAutofitColumnWidth(spreadsheet.getSelectedSheet().getInternalSheet(), 0, maxColumn);
     }
 }
