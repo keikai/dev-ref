55a94c0c338aaf668c6c15bd3f6f64e1840767ff
hawkchen <hawkchen@potix.com>
2021-12-22 15:47:49 +0800

    update autofit width and height

--- src/main/java/io/keikai/devref/component/AutoFitComposer.java
+++ src/main/java/io/keikai/devref/component/AutoFitComposer.java
@@ -1,10 +1,10 @@
 package io.keikai.devref.component;
 
-import io.keikai.api.Ranges;
+import io.keikai.api.*;
 import io.keikai.ui.Spreadsheet;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.*;
-import org.zkoss.zul.Intbox;
+import org.zkoss.zul.*;
 
 public class AutoFitComposer extends SelectorComposer {
     @Wire
@@ -13,6 +13,8 @@ public class AutoFitComposer extends SelectorComposer {
     private Intbox startCol;
     @Wire
     private Intbox endCol;
+    @Wire
+    private Checkbox useSelectionBox;
 
     @Listen("onClick = #autofit")
     public void autoFit(){
@@ -40,5 +42,44 @@ public class AutoFitComposer extends SelectorComposer {
             }
         }
         spreadsheet.setAutofitColumnWidth(spreadsheet.getSelectedSheet().getInternalSheet(), 0, maxColumn);
+    }
+
+    @Listen("onClick = #autofitHeight")
+    public void autoFitRowHeight() {
+        if (useSelectionBox.isChecked()) {
+            Range selectedRange = Ranges.range(spreadsheet.getSelectedSheet(), spreadsheet.getSelection());
+            selectedRange.zOrderStream().forEach(range -> {
+                if (range.hasMergedCell()) {
+                    //only auto-fit the first row of a merged cell
+                    autoFitMergedCell(range);
+                } else {
+                    autoFitOneCell(range);
+                }
+            });
+        }
+    }
+
+    private void autoFitOneCell(Range range) {
+        int currentHeight = range.getSheet().getRowHeight(range.getRow());
+        int autofitHeight = CellOperationUtil.getAutoFitHeight(spreadsheet.getSelectedSheet(),
+                range.getRow(), range.getColumn());
+        if (autofitHeight != currentHeight) {
+            range.setRowHeight(autofitHeight);
+        }
+    }
+
+    /**
+     * only change the first top cell of a vertical-merged cell
+     * @param mergedCell
+     */
+    private void autoFitMergedCell(Range mergedCell) {
+        //sum all rows' height except the 1st row
+        int heightSum = 0;
+        for (int r = mergedCell.getMergedRegion().getRow() + 1; r <= mergedCell.getMergedRegion().getLastRow(); r++) {
+            heightSum += mergedCell.getSheet().getRowHeight(r);
+        }
+        int autofitHeight = CellOperationUtil.getAutoFitHeight(spreadsheet.getSelectedSheet(),
+                mergedCell.getRow(), mergedCell.getColumn());
+        mergedCell.toCellRange(0, 0).setRowHeight(autofitHeight - heightSum);
     }
 }
