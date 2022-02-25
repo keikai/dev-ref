23bdc01a6a72c988d9f6f30ef74839423567e4b9
hawkchen <hawkchen@potix.com>
2022-02-25 16:16:48 +0800

    find a chart

--- src/main/java/io/keikai/devref/model/ChartComposer.java
+++ src/main/java/io/keikai/devref/model/ChartComposer.java
@@ -3,11 +3,12 @@ package io.keikai.devref.model;
 import io.keikai.api.*;
 import io.keikai.api.model.Chart;
 import io.keikai.api.model.Chart.*;
-import io.keikai.ui.Spreadsheet;
+import io.keikai.ui.*;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.util.Notification;
 import org.zkoss.zul.*;
 
 /**
@@ -19,6 +20,7 @@ import org.zkoss.zul.*;
 @SuppressWarnings("serial")
 public class ChartComposer extends SelectorComposer<Component> {
 
+	private static final AreaRef DEFAULT_DATA = new AreaRef("A1:B6");
 	@Wire
 	private Intbox toRowBox;
 	@Wire
@@ -37,51 +39,12 @@ public class ChartComposer extends SelectorComposer<Component> {
 		chartListbox.setModel(chartList);
 	}
 
-	public void add() {
-		Range dataRange = Ranges.range(ss.getSelectedSheet(),new AreaRef("A1:B6"));
-		SheetAnchor selectionAnchor = SheetOperationUtil.toChartAnchor(dataRange);
-		Chart chart = dataRange.addChart(selectionAnchor, Type.PIE, Grouping.STANDARD, LegendPosition.RIGHT);
-		refreshChartList();
-	}
 
-
-	public void delete() {
-		if (chartListbox.getSelectedItem() != null){
-			Ranges.range(ss.getSelectedSheet())
-				.deleteChart((Chart)chartListbox.getSelectedItem().getValue());
-			refreshChartList();
-		}
-	}
-
-	public void move() {
-		if (chartListbox.getSelectedItem() != null){
-			//calculate destination anchor
-			SheetAnchor fromAnchor = ((Chart) chartListbox.getSelectedItem()
-					.getValue()).getAnchor();
-			int rowOffset = fromAnchor.getLastRow() - fromAnchor.getRow();
-			int columnOffset = fromAnchor.getLastColumn() - fromAnchor.getColumn();
-			SheetAnchor toAnchor = new SheetAnchor(toRowBox.getValue(), 
-					toColumnBox.getValue(),
-					fromAnchor.getXOffset(), fromAnchor.getYOffset(),
-					toRowBox.getValue()+rowOffset, toColumnBox.getValue()+columnOffset,
-					fromAnchor.getLastXOffset(), fromAnchor.getLastYOffset());
-			
-			Ranges.range(ss.getSelectedSheet())
-				.moveChart(toAnchor, (Chart)chartListbox.getSelectedItem().getValue());
-			refreshChartList();
-		}
-	}
-
-	private void refreshChartList(){
-		chartList.clear();
-		chartList.addAll(ss.getSelectedSheet().getCharts());
-	}
-	
 	@Listen("onClick = #addButton")
 	public void addByUtil(){
 		AreaRef selection = ss.getSelection();
 		if (isOneCell(selection)){
-			selection = new AreaRef("A1:B6");
+			selection = DEFAULT_DATA;
 		}
 		SheetOperationUtil.addChart(Ranges.range(ss.getSelectedSheet(), selection),
 				Type.PIE, Grouping.STANDARD, LegendPosition.RIGHT);
@@ -108,6 +71,64 @@ public class ChartComposer extends SelectorComposer<Component> {
 		if (chartListbox.getSelectedItem() != null){
 			SheetOperationUtil.deleteChart(Ranges.range(ss.getSelectedSheet()), 
 					chartListbox.getSelectedItem().getValue());
+			refreshChartList();
+		}
+	}
+
+	@Listen("onClick = #find")
+	public void findBySelection(){
+		for (Chart chart : ss.getSelectedSheet().getCharts()){
+			AreaRef selection = ss.getSelection();
+			AreaRefWithType selectionArea = new AreaRefWithType(selection.getRow(), selection.getColumn(),
+					selection.getLastRow(), selection.getLastColumn(), CellSelectionType.ALL);
+
+			SheetAnchor anchor = chart.getAnchor();
+			AreaRefWithType chartArea = new AreaRefWithType(anchor.getRow(), anchor.getColumn(),
+					anchor.getLastRow(), anchor.getLastColumn(), CellSelectionType.ALL);
+			if (selectionArea.overlap(chartArea)){
+				chartList.addToSelection(chart);
+				return;
+			}
+		}
+		Notification.show("no chart found in the selection");
+	}
+
+	public void add() {
+		Range dataRange = Ranges.range(ss.getSelectedSheet(),new AreaRef("A1:B6"));
+		SheetAnchor selectionAnchor = SheetOperationUtil.toChartAnchor(dataRange);
+		Chart chart = dataRange.addChart(selectionAnchor, Type.PIE, Grouping.STANDARD, LegendPosition.RIGHT);
+		refreshChartList();
+	}
+
+
+	public void delete() {
+		if (chartListbox.getSelectedItem() != null){
+			Ranges.range(ss.getSelectedSheet())
+					.deleteChart((Chart)chartListbox.getSelectedItem().getValue());
+			refreshChartList();
+		}
+	}
+
+	private void refreshChartList(){
+		chartList.clear();
+		chartList.addAll(ss.getSelectedSheet().getCharts());
+	}
+
+	public void move() {
+		if (chartListbox.getSelectedItem() != null){
+			//calculate destination anchor
+			SheetAnchor fromAnchor = ((Chart) chartListbox.getSelectedItem()
+					.getValue()).getAnchor();
+			int rowOffset = fromAnchor.getLastRow() - fromAnchor.getRow();
+			int columnOffset = fromAnchor.getLastColumn() - fromAnchor.getColumn();
+			SheetAnchor toAnchor = new SheetAnchor(toRowBox.getValue(),
+					toColumnBox.getValue(),
+					fromAnchor.getXOffset(), fromAnchor.getYOffset(),
+					toRowBox.getValue()+rowOffset, toColumnBox.getValue()+columnOffset,
+					fromAnchor.getLastXOffset(), fromAnchor.getLastYOffset());
+
+			Ranges.range(ss.getSelectedSheet())
+					.moveChart(toAnchor, (Chart)chartListbox.getSelectedItem().getValue());
 			refreshChartList();
 		}
 	}
