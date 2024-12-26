9ff2d8d0fc2d6aeb6d60e877df74288b614f7f85
hawkchen <hawkchen@potix.com>
2024-12-26 16:05:40 +0800

    Analyzer: read zss.properties

--- src/main/java/io/keikai/devref/analyzer/Analyzer.java
+++ src/main/java/io/keikai/devref/analyzer/Analyzer.java
@@ -4,15 +4,18 @@ import io.keikai.api.Importers;
 import io.keikai.api.model.*;
 import io.keikai.devref.analyzer.counter.*;
 import io.keikai.model.*;
+import org.zkoss.lang.Library;
 
 import java.io.*;
-import java.util.Iterator;
+import java.util.*;
+
+import static java.lang.System.exit;
 
 /**
  * Iterate cells and generate a statistics report including the number of:
  * - sheets
  * - non-empty cells, formulas
- *  - TODO cells of each sheet
+ * - TODO cells of each sheet
  * - name ranges
  * - TODO images, charts
  * - TODO data validation
@@ -25,6 +28,7 @@ public class Analyzer {
     static private Counter<Book> bookCounter;
 
     public static void main(String[] args) throws IOException {
+        setup();
         checkArguments(args);
         importFile();
         setupCounter();
@@ -40,20 +44,20 @@ public class Analyzer {
     private static void setupCounter() {
         bookCounter = new StyleCounter(null);
         sheetCounter = new SheetCounter(
-                       new NameCounter(null));
+                new NameCounter(null));
         cellCounter = new CellCounter(
-                      new FormulaCounter(null));
+                new FormulaCounter(null));
     }
 
     private static void analyze() {
         bookCounter.process(book);
-        for (int i = 0 ; i < book.getNumberOfSheets() ; i++){
+        for (int i = 0; i < book.getNumberOfSheets(); i++) {
             Sheet sheet = book.getSheetAt(i);
             sheetCounter.process(sheet);
             Iterator<SRow> iterator = sheet.getInternalSheet().getRowIterator();
-            while (iterator.hasNext()){
+            while (iterator.hasNext()) {
                 Iterator<SCell> cellIterator = iterator.next().getCellIterator();
-                while (cellIterator.hasNext()){
+                while (cellIterator.hasNext()) {
                     cellCounter.process(cellIterator.next());
                 }
             }
@@ -61,11 +65,12 @@ public class Analyzer {
     }
 
     private static void checkArguments(String[] args) {
-        if (args.length<1){
+        if (args.length < 1) {
             System.out.println("a file with full path is required");
+            exit(1);
         }
         file = new File(args[0]);
-        if (!file.exists()){
+        if (!file.exists()) {
             System.out.println(file.getAbsolutePath() + " not found!");
             return;
         }
@@ -82,7 +87,7 @@ public class Analyzer {
 
     private static void printReportItem(Counter counter) {
         Counter currentCounter = counter;
-        while (currentCounter !=null) {
+        while (currentCounter != null) {
             ReportItem item = new ReportItem(currentCounter);
             System.out.printf("%15s : %s\n", item.getName(), item.getResult());
             currentCounter = currentCounter.getNext();
@@ -99,5 +104,20 @@ public class Analyzer {
 
     public static Counter<Book> getBookCounter() {
         return bookCounter;
+    }
+
+    private static void setup() {
+        Properties props = new Properties();
+        try {
+            props.load(Analyzer.class.getResourceAsStream("/zss.test.properties"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for (Iterator<?> it = props.keySet().iterator(); it.hasNext(); ) {
+            String key = (String) it.next();
+            Library.setProperty(key, props.getProperty(key));
+        }
     }
 }
